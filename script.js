7188dac405128afa610120e251a5cd14a19afa7a
melikeisikk <38049922+melikeisikk@users.noreply.github.com>
2018-04-03 18:21:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+function  changeColor(color) {
+    document.bgColor=color;
+
+}
+
+console.log("furkan");
+document.write("furkan");
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+document.write("hello world");
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+//escape chracter
+document.write("he said that: \"hello my friend\" ");
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+var name = "furkan kesgin";
+console.log(name);
+document.write(name);
+
+
+var number = 5;
+document.write("\n");
+document.write(number);
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+var num = -59.236985;
+document.write(num);
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+var job;
+job='doctor';
+document.write(job);
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+//boolean
+var man = false;
+document.write(man);
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+var naame=null;
+document.write(naame);
+
+document.write("</br>");
+document.write("</br>");
+document.write("</br>");
+
+
+
+/*  box == 7    integer
+    box == '7'  string
+    box === 7
+
+     */
+
+
+
+
+
+
